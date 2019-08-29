da9920828afa90faf42ae4410e20c0945ec9f803
Rodrigo <rodrigo.pires.net@gmail.com>
2019-08-29 03:29:57 -0300

    camelCase do diretorio

---
+++ src/components/editor/Editor.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './Editor';
+
+xit('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(
+    <Editor
+        theme="snow"
+        height="75vh"
+        onChange={() => { return null }}
+        text={"texto"}
+        readOnly={false} />, div);
+
+    ReactDOM.unmountComponentAtNode(div);
+})
